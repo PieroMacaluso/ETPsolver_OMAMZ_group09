544a0a9852457f2a9fa442d7ef349ee824d304ca
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-11-23 10:02:52 +0100

    Esperimenti matriciali

--- Java/src/it/polito/studenti/oma9/Main.java
+++ Java/src/it/polito/studenti/oma9/Main.java
@@ -1,11 +1,33 @@
 package it.polito.studenti.oma9;
 
-// import java.io.*;
+import java.io.*;
+//import java.io.BufferedReader;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello wooooorld");
+
+        int n = 5;
+        int m = 7;
+        int i, j;
+
+        int[][] cose = new int[n][m];
+
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < m; j++) {
+                cose[i][j] = i*j;
+            }
+        }
+
+
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < m; j++) {
+                System.out.print(cose[i][j]);
+                System.out.print(", ");
+            }
+            System.out.println("");
+        }
     }
 }
