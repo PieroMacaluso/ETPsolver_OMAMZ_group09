a5242f96ee1d5bb2fa5d43e58166572f93709f94
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-11-23 09:55:07 +0100

    Codice che funziona, yay

---
+++ Java/src/it/polito/studenti/oma9/Main.java
@@ -0,0 +1,11 @@
+package it.polito.studenti.oma9;
+
+// import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello wooooorld");
+    }
+}
