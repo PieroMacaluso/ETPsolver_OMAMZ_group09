2bb1c2b7b6be75a6a6bfaf57b401339a1f82c916
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-01-03 22:37:46 +0100

    Megabench migliorato

--- instances/megabench.php
+++ instances/megabench.php
@@ -1,14 +1,27 @@
 #!/usr/bin/php
 <?php
 
-function sig_handler($signo) {
-	if($signo === SIGINT) {
-		global $cose;
-		foreach($cose as $inst => $best) {
-			echo "Record istanza $inst: $best".PHP_EOL;
-		}
-		exit();
+function printresults() {
+	global $cose;
+	foreach($cose as $inst => $best) {
+		echo "Record istanza $inst: $best".PHP_EOL;
 	}
+	exit();
+}
+
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+	echo 'WINDOWS!'.PHP_EOL;
+	$windows = true;
+} else {
+	echo 'LINUX!'.PHP_EOL;
+	$windows = false;
+	function sig_handler($signo) {
+		if($signo === SIGINT) {
+			printresults();
+		}
+	}
+	pcntl_signal(SIGINT, "sig_handler");
+	declare(ticks=1);
 }
 
 $cose = [
@@ -21,13 +34,17 @@ $cose = [
 		7 => 9999.9
 		];
 $l = strlen("Final solution: ");
-pcntl_signal(SIGINT, "sig_handler");
-declare(ticks=1);
+
 
 while(true) {
 	foreach($cose as $inst => &$best) {
-		//exec("java -jar ../JavaSA/target/ETPsolver_OMAMZ_group09.jar instance0$inst -t 30", $output);
-		exec("java -jar ETPsolver_OMAMZ_group09.jar instance0$inst -t 30", $output);
+		exec("java -jar ../JavaSA/target/ETPsolver_OMAMZ_group09.jar instance0$inst -t 30", $output);
+		//exec("java -jar ETPsolver_OMAMZ_group09.jar instance0$inst -t 30", $output);
+
+		if($windows && file_exists('stop.txt')) {
+			echo PHP_EOL.'File di stop trovato :O'.PHP_EOL;
+			printresults();
+		}
 
 		$last = array_pop($output);
 		if(substr($last, 0, $l)) {
