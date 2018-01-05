0b0936681c8c78107bbad321806728059c194622
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-01-05 10:58:31 +0100

    Megabench che raccoglie statistiche

--- instances/megabench.php
+++ instances/megabench.php
@@ -2,9 +2,13 @@
 <?php
 
 function printresults() {
-	global $cose;
-	foreach($cose as $inst => $best) {
-		echo "Record istanza $inst: $best".PHP_EOL;
+	global $top, $best, $worst, $sum, $iterations;
+	echo PHP_EOL;
+	foreach($top as $inst => $record) {
+		$max = $worst[$inst];
+		$avg = $sum[$inst]/$iterations[$inst];
+		$min = $best[$inst];
+		echo "\033[0mIstanza $inst (MIN/AVG/MAX/RECORD): $min/$avg/$max/$record".PHP_EOL;
 	}
 	exit();
 }
@@ -24,20 +28,56 @@ if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 	declare(ticks=1);
 }
 
-$cose = [
-		1 => 9999.9,
-		2 => 9999.9,
-		3 => 9999.9,
-		4 => 9999.9,
-		5 => 9999.9,
-		6 => 9999.9,
-		7 => 9999.9
+$top = [
+		1 => 157.075,
+		2 => 36.466,
+		3 => 34.329,
+		4 => 8.264,
+		5 => 13.820,
+		6 => 3.506,
+		7 => 10.487
+		];
+$best = [
+		1 => 99999.9,
+		2 => 99999.9,
+		3 => 99999.9,
+		4 => 99999.9,
+		5 => 99999.9,
+		6 => 99999.9,
+		7 => 99999.9
+		];
+$worst = [
+		1 => 0,
+		2 => 0,
+		3 => 0,
+		4 => 0,
+		5 => 0,
+		6 => 0,
+		7 => 0
+		];
+$sum = [
+		1 => 0,
+		2 => 0,
+		3 => 0,
+		4 => 0,
+		5 => 0,
+		6 => 0,
+		7 => 0
+		];
+$iterations = [
+		1 => 0,
+		2 => 0,
+		3 => 0,
+		4 => 0,
+		5 => 0,
+		6 => 0,
+		7 => 0
 		];
 $l = strlen("Final solution: ");
 
 
 while(true) {
-	foreach($cose as $inst => &$best) {
+	foreach($best as $inst => &$topcurrent) {
 		exec("java -jar ../JavaSA/target/ETPsolver_OMAMZ_group09.jar instance0$inst -t 30", $output);
 		//exec("java -jar ETPsolver_OMAMZ_group09.jar instance0$inst -t 30", $output);
 
@@ -49,14 +89,35 @@ while(true) {
 		$last = array_pop($output);
 		if(substr($last, 0, $l)) {
 			$current = (double) substr($last, $l);
-			if($current < $best) {
-				$best = $current;
-				echo $inst;
-				//echo "Record istanza $inst: $current".PHP_EOL;
-				copy("instance0$inst.sol", "instance0$inst-best.sol");
-			} else {
-				echo ".";
+			if($current > $worst[$inst] && $current < $best[$inst]) {
+				$worst[$inst] = $current;
+				$best[$inst] = $current;
+				if($current < $topcurrent) {
+					$topcurrent = $current;
+					echo "\033[0;31m$inst";
+					copy("instance0$inst.sol", "instance0$inst-best.sol");
+					echo $windows ? 'R' : "\033[m\033[42m$inst\033[m";
+				} else {
+					echo $windows ? '.' : "\033[0;34m$inst";
+				}
+			} else if($current > $worst[$inst]) {
+				$worst[$inst] = $current;
+				echo $windows ? '.' : "\033[0;31m$inst";
+			} else if($current < $best[$inst]) {
+				$best[$inst] = $current;
+				if($current < $topcurrent) {
+					$topcurrent = $current;
+					echo "\033[0;31m$inst";
+					//echo "Record istanza $inst: $current".PHP_EOL;
+					copy("instance0$inst.sol", "instance0$inst-best.sol");
+					echo $windows ? 'R' : "\033[m\033[42m$inst\033[m";
+				} else {
+					echo $windows ? 'b' : "\033[0;32m$inst";
+				}
 			}
+			file_put_contents("log_$inst.txt", $current.PHP_EOL, FILE_APPEND);
+			$sum[$inst] += $current;
+			$iterations[$inst]++;
 		} else {
 			echo 'Errore: ' . $last . PHP_EOL;
 		}
