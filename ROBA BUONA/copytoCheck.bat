fd4cf5a1c07e3df91371fba3d11529d9426c9429
Piero Macaluso <piero.macaluso@studenti.polito.it>
2018-01-07 18:44:37 +0100

    Disparated things

--- ROBA BUONA/copytoCheck.bat
+++ ROBA BUONA/copytoCheck.bat
@@ -1,6 +1,6 @@
 @echo off
 FOR /L %%i IN (1,1,7) DO (
 	echo Instance 0%%i
-		copy instance0%%i.sol ..\instances\instance0%%i.sol
+		copy instance0%%i.sol ..\instances\instance0%%i_OMAMZ_group09.sol
 )
 pause
--- instances/bench.bat
+++ instances/bench.bat
@@ -1,10 +1,10 @@
 @echo off
-ETPchecker.exe instance01 -check .\instance01
-ETPchecker.exe instance02 -check .\instance02
-ETPchecker.exe instance03 -check .\instance03
-ETPchecker.exe instance04 -check .\instance04
-ETPchecker.exe instance05 -check .\instance05
-ETPchecker.exe instance06 -check .\instance06
-ETPchecker.exe instance07 -check .\instance07
+ETPchecker.exe instance01 -check .\instance01_OMAMZ_group09
+ETPchecker.exe instance02 -check .\instance02_OMAMZ_group09
+ETPchecker.exe instance03 -check .\instance03_OMAMZ_group09
+ETPchecker.exe instance04 -check .\instance04_OMAMZ_group09
+ETPchecker.exe instance05 -check .\instance05_OMAMZ_group09
+ETPchecker.exe instance06 -check .\instance06_OMAMZ_group09
+ETPchecker.exe instance07 -check .\instance07_OMAMZ_group09
 echo ciao
 pause
